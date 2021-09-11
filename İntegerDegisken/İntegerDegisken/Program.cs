0edbaefd78f332060c9c7d915d7b87599bf01a18
Doğukan <60947113+Dogukan18@users.noreply.github.com>
2021-09-11 09:48:40 +0300

    Add files via upload

---
+++ İntegerDegisken/İntegerDegisken/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+
+namespace İntegerDegisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int değişkeni tam sayılarda kullanılır
+
+            /*        int sayi1,sayi2,toplam;
+                      sayi1 = 5;
+                      sayi2 = 7;
+                      toplam = sayi1 + sayi2;
+                      Console.WriteLine(toplam);
+                      Console.Read(); */
+
+
+            //3 sayının ortalaması
+
+            /*      int a, b, c, ort;
+
+                    a = 4;
+                    b = 6;
+                    c = 2;
+                      ort = (a + b + c) / 3;
+                      Console.WriteLine(ort);
+                      Console.Read(); */
+
+            //Öğrenci bilgileri ve notlarının ortalamasının ekrana yansıtılması
+
+            /*       string ad, soyad, no;
+                       int sınav1, sınav2, sınav3, proje, ortalama;
+                       ad = "Doğukan";
+                       soyad = "Şengöz";
+                       no = "94";
+
+                       Console.WriteLine("****Öğrenci Bilgi****");
+                       Console.WriteLine();
+                       Console.WriteLine("Ad: " + ad);
+                       Console.WriteLine("Soyad: " + soyad);
+                       Console.WriteLine("No: " + no);
+                       Console.WriteLine();
+                       Console.WriteLine("****Not Bilgisi****");
+                       Console.WriteLine();
+
+                      sınav1= 75;
+                      sınav2 = 80;
+                      sınav3 = 85;
+                      proje = 90;
+                      ortalama = (sınav1 + sınav2 + sınav3 + proje) / 4;
+
+
+                       Console.WriteLine("Sınav 1: " + sınav1);
+                       Console.WriteLine("Sınav2: " + sınav2);
+                       Console.WriteLine("Sınav3: " + sınav3);
+                       Console.WriteLine("Proje: " + proje);
+                       Console.WriteLine();
+                       Console.WriteLine("****Ortalama****");
+                       Console.WriteLine("Ortalama: " + ortalama);
+                       Console.Read(); */
+
+
+            //Dışarıdan girilen 2 sayının toplamı
+            //Convert.ToInt32:Dışardan girilen değeri aritmetik işleme çevirme
+
+            /*         int sayi1, sayi2, toplam;
+                         Console.WriteLine("****Toplama İşlemi****");
+
+                         Console.WriteLine("1.Sayıyı Giriniz: ");
+                         sayi1 = Convert.ToInt32(Console.ReadLine());
+                         Console.WriteLine("2.Sayıyı Giriniz: ");
+                         sayi2 = Convert.ToInt32(Console.ReadLine());
+                         toplam = sayi1 + sayi2;
+                         Console.WriteLine("Toplam: " + toplam);
+                         Console.Read(); */
+
+            //Dışarıdan girilen 2 sayının toplam,fark,çarpım,bölümü
+
+            /*      int sayi1, sayi2, toplam, fark, carpim, bolum;
+                    Console.WriteLine("**** 4 İşlem ****");
+                    Console.WriteLine();
+
+                    Console.WriteLine("1.Sayıyı Giriniz: ");
+                    sayi1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("2.Sayıyı Giriniz: ");
+                    sayi2 = Convert.ToInt32(Console.ReadLine());
+
+                    toplam = sayi1 + sayi2;
+                    fark = sayi1 - sayi2;
+                    carpim = sayi1 * sayi2;
+                    bolum = sayi1 / sayi2;
+
+                    Console.WriteLine();
+                    Console.WriteLine("Toplam: " + toplam);
+                    Console.WriteLine("Fark: " + fark);
+                    Console.WriteLine("Çarpım: " + carpim);
+                    Console.WriteLine("Bölüm: " + bolum);
+
+                    Console.Read(); */
+
+            //Kare ve Dikdörtgenin alan ve çevresini hesaplayan program
+
+            /*    int a, b, karealan, karecevre, dikalan, dikcevre;
+                  Console.WriteLine("****Kare****");
+                  Console.WriteLine();
+                  Console.Write("Kenar Uzunluğunu Giriniz: ");
+                  a = Convert.ToInt32(Console.ReadLine());
+                  Console.WriteLine();
+                 karealan = a * a;
+                  Console.WriteLine("Karenin Alanı: " + karealan);
+                  karecevre = 4 * a;
+                  Console.Write("Karenin Çevresi: " + karecevre);
+                  Console.WriteLine();
+                  Console.WriteLine();
+                  Console.WriteLine("****Dikdörtgen****");
+                  Console.WriteLine();
+                  Console.Write("Uzun Kenarı Giriniz: ");
+                  a = Convert.ToInt32(Console.ReadLine());
+                  Console.Write("Kısa Kenarı Giriniz: ");
+                  b = Convert.ToInt32(Console.ReadLine());
+                 dikalan = a * b;
+                  Console.WriteLine();
+                  Console.WriteLine("Dikdörtgenin Alanı: " + dikalan);
+                  dikcevre = (a + b) * 2;
+                  Console.Write("Dikdörtgenin Çevresi: " + dikcevre);
+                  Console.Read();  */
+
+            
+
+      
+
+            
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
